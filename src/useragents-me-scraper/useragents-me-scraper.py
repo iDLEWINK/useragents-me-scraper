b97fa0a2fe76f3459d79fc6a4295b6b8c28165c9
iDLEWINK <josh_uson@dlsu.edu.ph>
2023-05-05 01:01:51 +0800

    feat: initialize required python files

---
+++ src/useragents-me-scraper/useragents-me-scraper.py
@@ -0,0 +1,4 @@
+# Define checking of existing
+# Define saving of JSON
+# Define basic scraping/fetching of JSON
+# Define main functionality of getting UA with specifications
---
+++ src/useragents-me-scraper/utils.py
@@ -0,0 +1,1 @@
+# Define time of checking here
