aa37cc9e68de8c476b37a4304cb5706a127880c4
iDLEWINK <josh_uson@dlsu.edu.ph>
2023-05-06 20:22:11 +0800

    feat: add priv check_ua_cache func

--- src/useragents-me-scraper/useragents-me-scraper.py
+++ src/useragents-me-scraper/useragents-me-scraper.py
@@ -5,13 +5,22 @@ from bs4 import BeautifulSoup
 # Define saving of JSON
 
 
-def _save_ua_cache():
+def _save_ua_cache(ua_processed_json):
     FILENAME = 'uacache.json'
     with open(FILENAME, 'w') as outfile:
-        json.dump(ua_json, outfile)
+        json.dump(ua_processed_json, outfile)
 
 # Define checking of existing JSON
 
+
+def _check_existing_ua_cache():
+    existing_flag = True
+    try:
+        f = open('uacache.json')
+    except:
+        print('A local ua_cache file does not exist.')
+        existing_flag = False
+    return existing_flag
 # Define basic scraping/fetching of JSON
 
 # Define main functionality of getting UA with specifications
