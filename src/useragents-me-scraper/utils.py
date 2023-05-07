f2a2a275f1be58b3a8db0ce3acbdfd234d621212
iDLEWINK <josh_uson@dlsu.edu.ph>
2023-05-07 10:09:17 +0800

    refactor: create separate function for convert_str_to_date

--- src/useragents-me-scraper/utils.py
+++ src/useragents-me-scraper/utils.py
@@ -1,6 +1,17 @@
 from datetime import datetime, date, timedelta
 
 
+def convert_str_to_date(str_date):
+    """ Converts a string to a _Date object and returns it
+
+    Returns
+    -------
+    _Date 
+      Formatted date to YYYY-MM-DD format
+    """
+    return datetime.strptime(str_date, '%Y-%m-%d').date()
+
+
 def get_week_timeframe():
     """ Returns two _Date values - start_date (today) and end_date (date 7 days after start_date)
 
@@ -30,5 +41,5 @@ def is_outdated(end_date):
       True if the date is outdated. False if the date is not outdated.
     """
 
-    end_date = datetime.strptime(str(end_date), '%Y-%m-%d').date()
+    end_date = convert_str_to_date(end_date)
     return date.today() > end_date
