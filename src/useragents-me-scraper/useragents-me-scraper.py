2853d4a735553695d9fd66908e22359f7959817a
iDLEWINK <josh_uson@dlsu.edu.ph>
2023-05-07 10:10:13 +0800

    feat: add is_outdated private function

--- src/useragents-me-scraper/useragents-me-scraper.py
+++ src/useragents-me-scraper/useragents-me-scraper.py
@@ -18,11 +18,22 @@ def _is_existing_ua_cache():
     existing_flag = True
     try:
         f = open('ua_cache.json')
+        f.close()
     except:
         print('A local ua_cache.json file does not exist.')
         existing_flag = False
     return existing_flag
 
+
+def _is_outdated_ua_cache():
+    outdated_flag = True
+    try:
+        f = open('uacache.json')
+        ua_data = json.load(f)
+        outdated_flag = utils.is_outdated(ua_data['end_date'])
+    except:
+        print('An exception concerning the ua_cache.json file has occurred.')
+    return outdated_flag
 # Define basic scraping/fetching of JSON
 
 
@@ -42,3 +53,10 @@ def _scrape_ua_me():
 
 
 def get_uas():
+    if not _is_existing_ua_cache() or _is_outdated_ua_cache():
+        # Need to process
+        ua_raw_json = _scrape_ua_me()
+
+        # Need to save
+
+    # Need to check the expiry
